392fe47c195a63a26bad4e35a357d5248deb6cd5
tatomr <roderick.tato@gmail.com>
2018-05-03 20:40:20 -0400

    V5

---
+++ Array Assignment 4_30_2018 v5/Array Assignment 4_30_2018/Array File Reader Assignment/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace Array_File_Reader_Assignment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sortToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.searchBox1 = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 29);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(207, 251);
+            this.listBox1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(475, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sortToolStripMenuItem,
+            this.exportToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.fileToolStripMenuItem.Text = "Options";
+            // 
+            // sortToolStripMenuItem
+            // 
+            this.sortToolStripMenuItem.Name = "sortToolStripMenuItem";
+            this.sortToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.sortToolStripMenuItem.Text = "Sort";
+            this.sortToolStripMenuItem.Click += new System.EventHandler(this.sortToolStripMenuItem_Click);
+            // 
+            // exportToolStripMenuItem
+            // 
+            this.exportToolStripMenuItem.Name = "exportToolStripMenuItem";
+            this.exportToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.exportToolStripMenuItem.Text = "Export";
+            this.exportToolStripMenuItem.Click += new System.EventHandler(this.exportToolStripMenuItem_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(225, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 37);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Location = new System.Drawing.Point(225, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 37);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(225, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Load Speed";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(222, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Sort Speed";
+            // 
+            // searchBox1
+            // 
+            this.searchBox1.Location = new System.Drawing.Point(228, 260);
+            this.searchBox1.Name = "searchBox1";
+            this.searchBox1.Size = new System.Drawing.Size(235, 20);
+            this.searchBox1.TabIndex = 7;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(312, 286);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 8;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(225, 174);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(75, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Search Speed";
+            // 
+            // label5
+            // 
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label5.Location = new System.Drawing.Point(225, 197);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(128, 37);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "label5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 426);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.searchBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sortToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ToolStripMenuItem exportToolStripMenuItem;
+        private System.Windows.Forms.TextBox searchBox1;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Array Assignment 4_30_2018 v5/Array Assignment 4_30_2018/Array File Reader Assignment/Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace Array_File_Reader_Assignment
+{
+    public partial class Form1 : Form
+    {
+
+        const string pathFinder = "Names.csv";
+
+        const int SIZE = 5000;
+        string[] iArray = new string[SIZE];
+        //Implements a TextReader that reads characters from a byte stream in a particular encoding.
+        StreamReader source = File.OpenText("Names.csv");
+
+
+        int counter;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {   //Gets a DateTime object that is set to the current date and time on this computer, expressed as the local time.
+            DateTime start = DateTime.Now;
+            DateTime Finish;
+            TimeSpan Time;
+            counter = 0;
+
+            try
+            {
+
+                // Increment my i variable
+                for (counter = 0; counter < 5000; counter++)
+                {
+                    // Adds each line that my stream reader reads
+                    // into the array instance of i
+                    iArray[counter] = source.ReadLine();
+                    // adds the instance of i into the list box
+                    listBox1.Items.Add(iArray[counter]);
+
+                }
+                Finish = DateTime.Now;
+                Time = Finish - start;
+
+                label1.Text = listBox1.Items.Count.ToString() + " " + "Results Loaded in" + " " + (Time.TotalSeconds.ToString()) + " " + "seconds";
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void SelectionSort(string[] iArray)
+        {
+            try
+            {
+
+            int minIndex;
+            string minValue;
+
+            for (int startScan = 0; startScan < iArray.Length - 1; startScan++)
+            {
+                minIndex = startScan;
+                minValue = iArray[startScan];
+
+                for (int index = startScan + 1; index < iArray.Length; index++)
+                {
+                    if (string.Compare(minValue, iArray[index], true) == 1)
+                    {
+                        minValue = iArray[index];
+                        minIndex = index;
+                    }
+                }
+
+                Swap(ref iArray[minIndex], ref iArray[startScan]);
+
+            }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Something went wrong in the sorting process...");
+            }
+        }
+
+        private void Swap(ref string a, ref string b)
+
+        {
+            string temp = a;
+            a = b;
+            b = temp;
+        }
+
+        private void sortToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                listBox1.Items.Clear();
+
+                DateTime start = DateTime.Now;
+                DateTime Finish;
+                TimeSpan Time;
+
+                while (counter < iArray.Length && !source.EndOfStream)
+                {
+                    iArray[counter] = source.ReadLine();
+                    counter++;
+                }
+
+                listBox1.Items.Clear();
+                SelectionSort(iArray);
+
+                foreach (string value in iArray)
+                {
+                    listBox1.Items.Add(value);
+                }
+
+                Finish = DateTime.Now;
+                Time = Finish - start;
+
+                label2.Text = listBox1.Items.Count.ToString()
+                    + " " + "Results Sorted in" + " " + (Time.TotalSeconds.ToString()) + " " + "seconds";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    
+
+        private void exportToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+            
+            string pathFinder2 = "sortedNames.csv";
+            // Stream Writer to write the text files
+            StreamWriter streamWriter = new StreamWriter(pathFinder2);
+            // Writes the sorted values into the new file     
+            foreach (string name in listBox1.Items)
+            {
+                streamWriter.WriteLine(name.ToString());
+            }
+                MessageBox.Show("File as been exported...");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error exporting file");      
+            }
+
+      
+        }
+        private int BinarySearch(string[] iArray, string value)
+        {
+            int first = 0;                      // first array element
+            int last = iArray.Length - 1;       // last array element
+            int middle;                         // Midpoint of search
+            int position = -1;                  // position of search value
+            bool found = false;                 // Flag
+
+            // Search for the value
+            while (!found && first <= last)
+            {
+                // Calc the midpoint.
+                middle = (first + last) / 2;
+
+                // If value is found at midpoint ...
+                if (iArray[middle] == value)
+                {
+                    found = true;
+                    position = middle;
+                }
+
+                // else if value is in lower half...
+                else if (string.Compare(iArray[middle], value, false) > 0)
+                {
+                    last = middle - 1;
+                }
+
+                // else if value is in upper half...
+                else
+                {
+                    first = middle + 1;
+                }
+            }
+            // Return the position of the item, or -1
+            // If it was not found.
+            return position;
+            
+        }
+        
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Time variables
+                DateTime start = DateTime.Now;
+                DateTime Finish;
+                TimeSpan Time;
+
+                // name = text in textbox
+                string Name = searchBox1.Text;
+                // sets the position
+                int position = BinarySearch(iArray, searchBox1.Text);
+                // boolean determining if name is in the array
+                bool Found = iArray.Contains(Name);
+                // If it was not found, display this message
+
+                if (position >= 0)
+                {
+                    Found = true;
+                }
+
+                if (Found == false)
+                {
+                    label5.Text = "This name could not be found";
+                }
+                // If found, do this
+                
+                else
+                {
+                    // Time stuff
+                    Finish = DateTime.Now;
+                    Time = Finish - start;
+                    // Highlights the name
+
+                    MessageBox.Show("This name was found at index # " + position);
+                    listBox1.SetSelected(position, true);
+                    string text = listBox1.GetItemText(listBox1.SelectedItem);
+                    label5.Text = text + " " + "found in" + " " + (Time.TotalSeconds.ToString()) + " " + "seconds";
+                }
+            }
+            catch
+            {  // Sam did it
+                MessageBox.Show("You broke it");
+            }
+        }
+    }
+}
+
+
+        
+
+
+  
+    
